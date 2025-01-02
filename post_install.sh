9e6d6cb2f35bda939e16f637428f78d00c7af67e
Javid Ahmed <javidahmed@icloud.com>
2025-01-02 23:07:41 +0000

    Update build process to use wheel file and remove tarball creation

--- post_install.sh
+++ post_install.sh
@@ -5,18 +5,10 @@ set -eu
 
 BIN_DIR="${PREFIX}/app"
 
-echo "Unpacking RPi-AI..."
-tar -xzf "${PREFIX}/rpi-ai.tar.gz" --directory="${PREFIX}"
-mv "${PREFIX}/rpi-ai" "${BIN_DIR}"
-rm "${PREFIX}/rpi-ai.tar.gz"
-
 echo "Installing from wheel..."
-
-echo "BIN_DIR:"
-ls -la ${BIN_DIR}
-PACKAGE_WHEEL=$(find ${BIN_DIR} -name 'rpi-ai-*-py3-none-any.whl')
-"${PREFIX}/bin/pip" install "${PACKAGE_WHEEL}"
-rm "${PACKAGE_WHEEL}"
+WHEEL_FILE=$(ls rpi-ai-*-py3-none-any.whl)
+"${PREFIX}/bin/pip" install "${WHEEL_FILE}"
+rm "${WHEEL_FILE}"
 
 echo "Creating API executable..."
 "${PREFIX}/bin/create-wrappers" -t conda --bin-dir "${PREFIX}/bin" --conda-env-dir "${PREFIX}" --dest-dir "${PREFIX}/wrappers/bin"
@@ -24,7 +16,7 @@ echo "Creating API executable..."
 EXEC_FILE="${BIN_DIR}/rpi-ai"
 cat > $EXEC_FILE << EOF
 #!/bin/bash
-"${PREFIX}/wrappers/bin/run-in" python -m src.app "\$@"
+"${PREFIX}/wrappers/bin/run-in" python -m run_rpi_ai
 EOF
 chmod +x $EXEC_FILE
 
