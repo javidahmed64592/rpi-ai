7eae0f6286c936a56768ff91a0fe6cbcf309b50a
Javid Ahmed <javidahmed@icloud.com>
2025-01-03 19:43:59 +0000

    Refactor chatbot initialization by moving logic from App class to main function

--- src/rpi_ai/main.py
+++ src/rpi_ai/main.py
@@ -1,6 +1,26 @@
-from rpi_ai.app import App
+import os
+
+from dotenv import load_dotenv
+
+from rpi_ai.models.chatbot import Chatbot
+from rpi_ai.models.logger import Logger
 
 
 def run() -> None:
-    app = App()
-    app.run()
+    logger = Logger(__name__)
+    logger.debug("Loading environment variables...")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    logger.debug("Loading config...")
+    model = "gemini-1.5-flash"
+
+    if not api_key:
+        msg = "GEMINI_API_KEY variable not set!"
+        logger.error(msg)
+        raise ValueError(msg)
+
+    chatbot = Chatbot(api_key, model)
+
+    response = chatbot.chat(str(input("User: ")))
+    logger.info(response)
