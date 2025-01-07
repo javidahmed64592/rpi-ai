fa2ed0d4d1f767bd753f54273fda4537a2cd321e
Javid Ahmed <javidahmed@icloud.com>
2025-01-07 00:11:36 +0000

    Add periodic API connectivity checks in HomePage

--- ui/lib/pages/home_page.dart
+++ ui/lib/pages/home_page.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ui/app_state.dart';
 import 'package:ui/components/settings_dialog.dart';
+import 'package:ui/helpers/http_helper.dart';
 import 'package:ui/pages/login_page.dart';
 import 'package:ui/pages/message_page.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,6 +16,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+    startCheckAPIAlive();
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  void startCheckAPIAlive() {
+    timer = Timer.periodic(const Duration(minutes: 1), (timer) {
+      final httpHelper = HttpHelper(client: http.Client());
+      httpHelper.checkApiConnection(context);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final appState = Provider.of<AppState>(context);
