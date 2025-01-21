097f56b9ab42d4ce842b51c58a839b59a2a7fa49
Javid Ahmed <javidahmed@icloud.com>
2025-01-21 01:00:40 +0000

    Add dispose method to ConversationPage to clean up scroll controller

--- ui/lib/pages/conversation_page.dart
+++ ui/lib/pages/conversation_page.dart
@@ -31,6 +31,12 @@ class _ConversationPageState extends State<ConversationPage> {
   }
 
   @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
