2042f88d5fa20e49196e0795cd83efc2a43e624b
Javid Ahmed <javidahmed@icloud.com>
2025-01-29 22:50:45 +0000

    Add SettingsButton to CustomAppBar and update tests for visibility

--- ui/test/components/app_bar/custom_app_bar_test.dart
+++ ui/test/components/app_bar/custom_app_bar_test.dart
@@ -8,6 +8,7 @@ import 'package:provider/provider.dart';
 // Project imports:
 import 'package:ui/components/app_bar/custom_app_bar.dart';
 import 'package:ui/components/app_bar/logout_button.dart';
+import 'package:ui/components/app_bar/settings_dialog.dart';
 import 'package:ui/components/app_bar/switch_chat_mode.dart';
 import 'package:ui/state/app_state.dart';
 import 'package:ui/types.dart';
@@ -46,6 +47,22 @@ void main() {
     appState.setActivePage(PageType.command);
     await tester.pumpWidget(createCustomAppBar(appState));
     expect(find.text(PageType.command.title), findsOneWidget);
+  });
+
+  testWidgets('CustomAppBar displays SettingsButton',
+      (WidgetTester tester) async {
+    final appState = AppState();
+    appState.setActivePage(PageType.chat);
+    await tester.pumpWidget(createCustomAppBar(appState));
+    expect(find.byType(SettingsButton), findsOneWidget);
+  });
+
+  testWidgets('CustomAppBar does not display SettingsButton on login page',
+      (WidgetTester tester) async {
+    final appState = AppState();
+    appState.setActivePage(PageType.login);
+    await tester.pumpWidget(createCustomAppBar(appState));
+    expect(find.byType(SettingsButton), findsNothing);
   });
 
   testWidgets('CustomAppBar displays SwitchChatMode',
