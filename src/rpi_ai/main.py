6bd14011508165bc0941969828e208773c36510f
Javid Ahmed <javidahmed@icloud.com>
2025-01-29 22:32:35 +0000

    Remove unused import of AIConfigType from main.py

--- src/rpi_ai/main.py
+++ src/rpi_ai/main.py
@@ -6,7 +6,6 @@ from types import FrameType
 from dotenv import load_dotenv
 from flask import Flask, Response, jsonify, request
 
-from rpi_ai.config import AIConfigType
 from rpi_ai.functions import FUNCTIONS
 from rpi_ai.models.chatbot import Chatbot
 from rpi_ai.models.logger import Logger
