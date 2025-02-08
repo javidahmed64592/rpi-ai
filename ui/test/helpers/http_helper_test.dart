fca0e9394002eec01d4d24adb438b7fdd3d08766
Javid Ahmed <javidahmed@icloud.com>
2025-02-08 23:57:32 +0000

    Remove group

--- ui/test/helpers/http_helper_test.dart
+++ ui/test/helpers/http_helper_test.dart
@@ -25,238 +25,235 @@ void main() {
     client = MockClient();
   });
 
-  group('HttpHelper', () {
-    testWidgets(
-        'checkApiConnection sets activePage to login if the http call completes with an error',
-        (WidgetTester tester) async {
-      final httpHelper = HttpHelper(client: client);
-      final appState = AppState();
-      const uri = '/';
+  testWidgets(
+      'checkApiConnection sets activePage to login if the http call completes with an error',
+      (WidgetTester tester) async {
+    final httpHelper = HttpHelper(client: client);
+    final appState = AppState();
+    const uri = '/';
 
-      when(client.get(Uri.parse(uri)))
-          .thenAnswer((_) async => http.Response('Not Found', 404));
+    when(client.get(Uri.parse(uri)))
+        .thenAnswer((_) async => http.Response('Not Found', 404));
 
-      await tester.pumpWidget(
-        ChangeNotifierProvider<AppState>.value(
-          value: appState,
-          child: Builder(
-            builder: (context) {
-              Future.microtask(() => httpHelper.checkApiConnection(uri));
-              return Container();
-            },
-          ),
+    await tester.pumpWidget(
+      ChangeNotifierProvider<AppState>.value(
+        value: appState,
+        child: Builder(
+          builder: (context) {
+            Future.microtask(() => httpHelper.checkApiConnection(uri));
+            return Container();
+          },
         ),
-      );
+      ),
+    );
 
-      await tester.pump();
+    await tester.pump();
 
-      expect(appState.activePage, PageType.login);
+    expect(appState.activePage, PageType.login);
+  });
+
+  test(
+      'checkApiConnection returns true if the http call completes successfully',
+      () async {
+    final httpHelper = HttpHelper(client: client);
+    const uri = 'http://example.com';
+
+    when(client.get(Uri.parse('$uri/'), headers: anyNamed('headers')))
+        .thenAnswer((_) async => http.Response('OK', 200));
+
+    expect(await httpHelper.checkApiConnection(uri), true);
+  });
+
+  test(
+      'checkApiConnection returns false if the http call completes with an error',
+      () async {
+    final httpHelper = HttpHelper(client: client);
+    const uri = 'http://example.com';
+
+    when(client.get(Uri.parse('$uri/'), headers: anyNamed('headers')))
+        .thenAnswer((_) async => http.Response('Not Found', 404));
+
+    expect(await httpHelper.checkApiConnection(uri), false);
+  });
+
+  test(
+      'getLoginResponse returns a message if the http call completes successfully',
+      () async {
+    final httpHelper = HttpHelper(client: client);
+    const uri = 'http://example.com';
+    const authToken = 'testToken';
+
+    when(client.get(
+      Uri.parse('$uri/login'),
+      headers: {'Authorization': authToken},
+    )).thenAnswer((_) async => http.Response(
+        jsonEncode({'message': 'Welcome', 'is_user_message': true}), 200));
+
+    expect(await httpHelper.getLoginResponse(uri, authToken), {
+      'text': 'Welcome',
+      'isUserMessage': true,
+      'timestamp': isA<DateTime>(),
     });
+  });
 
-    test(
-        'checkApiConnection returns true if the http call completes successfully',
-        () async {
-      final httpHelper = HttpHelper(client: client);
-      const uri = 'http://example.com';
+  test(
+      'getLoginResponse throws an exception if the http call completes with an error',
+      () {
+    final httpHelper = HttpHelper(client: client);
+    const uri = 'http://example.com';
+    const authToken = 'testToken';
 
-      when(client.get(Uri.parse('$uri/'), headers: anyNamed('headers')))
-          .thenAnswer((_) async => http.Response('OK', 200));
+    when(client.get(
+      Uri.parse('$uri/login'),
+      headers: {'Authorization': authToken},
+    )).thenAnswer((_) async => http.Response('Not Found', 404));
 
-      expect(await httpHelper.checkApiConnection(uri), true);
+    expect(httpHelper.getLoginResponse(uri, authToken), throwsException);
+  });
+
+  test('getConfig returns config if the http call completes successfully',
+      () async {
+    final httpHelper = HttpHelper(client: client);
+    const uri = 'http://example.com';
+    const authToken = 'testToken';
+
+    when(client.get(
+      Uri.parse('$uri/get-config'),
+      headers: {'Authorization': authToken},
+    )).thenAnswer((_) async => http.Response(
+        jsonEncode({
+          'model': 'testModel',
+          'system_instruction': 'testInstruction',
+          'candidate_count': 5,
+          'max_output_tokens': 100,
+          'temperature': 0.7,
+        }),
+        200));
+
+    expect(await httpHelper.getConfig(uri, authToken), {
+      'model': 'testModel',
+      'systemInstruction': 'testInstruction',
+      'candidateCount': 5,
+      'maxOutputTokens': 100,
+      'temperature': 0.7,
     });
+  });
 
-    test(
-        'checkApiConnection returns false if the http call completes with an error',
-        () async {
-      final httpHelper = HttpHelper(client: client);
-      const uri = 'http://example.com';
+  test('getConfig throws an exception if the http call completes with an error',
+      () {
+    final httpHelper = HttpHelper(client: client);
+    const uri = 'http://example.com';
+    const authToken = 'testToken';
 
-      when(client.get(Uri.parse('$uri/'), headers: anyNamed('headers')))
-          .thenAnswer((_) async => http.Response('Not Found', 404));
+    when(client.get(
+      Uri.parse('$uri/get-config'),
+      headers: {'Authorization': authToken},
+    )).thenAnswer((_) async => http.Response('Not Found', 404));
 
-      expect(await httpHelper.checkApiConnection(uri), false);
+    expect(httpHelper.getConfig(uri, authToken), throwsException);
+  });
+
+  test(
+      'updateConfig returns the first message if the http call completes successfully',
+      () async {
+    final httpHelper = HttpHelper(client: client);
+    const uri = 'http://example.com';
+    const authToken = 'testToken';
+    final config = {
+      'model': 'newModel',
+      'system_instruction': 'newInstruction',
+      'candidate_count': 10,
+      'max_output_tokens': 200,
+      'temperature': 0.9,
+    };
+
+    when(client.post(
+      Uri.parse('$uri/update-config'),
+      headers: {
+        'Authorization': authToken,
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(config),
+    )).thenAnswer((_) async => http.Response(
+        jsonEncode({
+          'message': 'Config updated successfully',
+          'is_user_message': false,
+        }),
+        200));
+
+    expect(await httpHelper.updateConfig(uri, authToken, config), {
+      'text': 'Config updated successfully',
+      'isUserMessage': false,
+      'timestamp': isA<DateTime>(),
     });
+  });
 
-    test(
-        'getLoginResponse returns a message if the http call completes successfully',
-        () async {
-      final httpHelper = HttpHelper(client: client);
-      const uri = 'http://example.com';
-      const authToken = 'testToken';
+  test(
+      'updateConfig throws an exception if the http call completes with an error',
+      () {
+    final httpHelper = HttpHelper(client: client);
+    const uri = 'http://example.com';
+    const authToken = 'testToken';
+    final config = {
+      'model': 'newModel',
+      'system_instruction': 'newInstruction',
+      'candidate_count': 10,
+      'max_output_tokens': 200,
+      'temperature': 0.9,
+    };
 
-      when(client.get(
-        Uri.parse('$uri/login'),
-        headers: {'Authorization': authToken},
-      )).thenAnswer((_) async => http.Response(
-          jsonEncode({'message': 'Welcome', 'is_user_message': true}), 200));
+    when(client.post(
+      Uri.parse('$uri/update-config'),
+      headers: {
+        'Authorization': authToken,
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(config),
+    )).thenAnswer((_) async => http.Response('Not Found', 404));
 
-      expect(await httpHelper.getLoginResponse(uri, authToken), {
-        'text': 'Welcome',
-        'isUserMessage': true,
-        'timestamp': isA<DateTime>(),
-      });
+    expect(httpHelper.updateConfig(uri, authToken, config), throwsException);
+  });
+
+  test('chat returns a message if the http call completes successfully',
+      () async {
+    final httpHelper = HttpHelper(client: client);
+    const uri = 'http://example.com';
+    const message = 'Hello';
+    const authToken = 'testToken';
+
+    when(client.post(
+      Uri.parse('$uri/chat'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': authToken,
+      },
+      body: jsonEncode({'message': message}),
+    )).thenAnswer((_) async => http.Response(
+        jsonEncode({'message': 'Hi', 'is_user_message': false}), 200));
+
+    expect(await httpHelper.chat(uri, authToken, message), {
+      'text': 'Hi',
+      'isUserMessage': false,
+      'timestamp': isA<DateTime>(),
     });
+  });
 
-    test(
-        'getLoginResponse throws an exception if the http call completes with an error',
-        () {
-      final httpHelper = HttpHelper(client: client);
-      const uri = 'http://example.com';
-      const authToken = 'testToken';
+  test('chat returns empty dict if the http call completes with an error',
+      () async {
+    final httpHelper = HttpHelper(client: client);
+    const uri = 'http://example.com';
+    const message = 'Hello';
+    const authToken = 'testToken';
 
-      when(client.get(
-        Uri.parse('$uri/login'),
-        headers: {'Authorization': authToken},
-      )).thenAnswer((_) async => http.Response('Not Found', 404));
+    when(client.post(
+      Uri.parse('$uri/chat'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': authToken,
+      },
+      body: jsonEncode({'message': message}),
+    )).thenAnswer((_) async => http.Response('Not Found', 404));
 
-      expect(httpHelper.getLoginResponse(uri, authToken), throwsException);
-    });
-
-    test('getConfig returns config if the http call completes successfully',
-        () async {
-      final httpHelper = HttpHelper(client: client);
-      const uri = 'http://example.com';
-      const authToken = 'testToken';
-
-      when(client.get(
-        Uri.parse('$uri/get-config'),
-        headers: {'Authorization': authToken},
-      )).thenAnswer((_) async => http.Response(
-          jsonEncode({
-            'model': 'testModel',
-            'system_instruction': 'testInstruction',
-            'candidate_count': 5,
-            'max_output_tokens': 100,
-            'temperature': 0.7,
-          }),
-          200));
-
-      expect(await httpHelper.getConfig(uri, authToken), {
-        'model': 'testModel',
-        'systemInstruction': 'testInstruction',
-        'candidateCount': 5,
-        'maxOutputTokens': 100,
-        'temperature': 0.7,
-      });
-    });
-
-    test(
-        'getConfig throws an exception if the http call completes with an error',
-        () {
-      final httpHelper = HttpHelper(client: client);
-      const uri = 'http://example.com';
-      const authToken = 'testToken';
-
-      when(client.get(
-        Uri.parse('$uri/get-config'),
-        headers: {'Authorization': authToken},
-      )).thenAnswer((_) async => http.Response('Not Found', 404));
-
-      expect(httpHelper.getConfig(uri, authToken), throwsException);
-    });
-
-    test(
-        'updateConfig returns the first message if the http call completes successfully',
-        () async {
-      final httpHelper = HttpHelper(client: client);
-      const uri = 'http://example.com';
-      const authToken = 'testToken';
-      final config = {
-        'model': 'newModel',
-        'system_instruction': 'newInstruction',
-        'candidate_count': 10,
-        'max_output_tokens': 200,
-        'temperature': 0.9,
-      };
-
-      when(client.post(
-        Uri.parse('$uri/update-config'),
-        headers: {
-          'Authorization': authToken,
-          'Content-Type': 'application/json',
-        },
-        body: jsonEncode(config),
-      )).thenAnswer((_) async => http.Response(
-          jsonEncode({
-            'message': 'Config updated successfully',
-            'is_user_message': false,
-          }),
-          200));
-
-      expect(await httpHelper.updateConfig(uri, authToken, config), {
-        'text': 'Config updated successfully',
-        'isUserMessage': false,
-        'timestamp': isA<DateTime>(),
-      });
-    });
-
-    test(
-        'updateConfig throws an exception if the http call completes with an error',
-        () {
-      final httpHelper = HttpHelper(client: client);
-      const uri = 'http://example.com';
-      const authToken = 'testToken';
-      final config = {
-        'model': 'newModel',
-        'system_instruction': 'newInstruction',
-        'candidate_count': 10,
-        'max_output_tokens': 200,
-        'temperature': 0.9,
-      };
-
-      when(client.post(
-        Uri.parse('$uri/update-config'),
-        headers: {
-          'Authorization': authToken,
-          'Content-Type': 'application/json',
-        },
-        body: jsonEncode(config),
-      )).thenAnswer((_) async => http.Response('Not Found', 404));
-
-      expect(httpHelper.updateConfig(uri, authToken, config), throwsException);
-    });
-
-    test('chat returns a message if the http call completes successfully',
-        () async {
-      final httpHelper = HttpHelper(client: client);
-      const uri = 'http://example.com';
-      const message = 'Hello';
-      const authToken = 'testToken';
-
-      when(client.post(
-        Uri.parse('$uri/chat'),
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': authToken,
-        },
-        body: jsonEncode({'message': message}),
-      )).thenAnswer((_) async => http.Response(
-          jsonEncode({'message': 'Hi', 'is_user_message': false}), 200));
-
-      expect(await httpHelper.chat(uri, authToken, message), {
-        'text': 'Hi',
-        'isUserMessage': false,
-        'timestamp': isA<DateTime>(),
-      });
-    });
-
-    test('chat returns empty dict if the http call completes with an error',
-        () async {
-      final httpHelper = HttpHelper(client: client);
-      const uri = 'http://example.com';
-      const message = 'Hello';
-      const authToken = 'testToken';
-
-      when(client.post(
-        Uri.parse('$uri/chat'),
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': authToken,
-        },
-        body: jsonEncode({'message': message}),
-      )).thenAnswer((_) async => http.Response('Not Found', 404));
-
-      expect(await httpHelper.chat(uri, authToken, message), {});
-    });
+    expect(await httpHelper.chat(uri, authToken, message), {});
   });
 }
