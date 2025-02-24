f2ba802f1ea27c373d5957f32f9d694779c97221
Javid Ahmed <javidahmed@icloud.com>
2025-02-24 01:18:42 +0000

    Remove logger initialization from functions.py to streamline imports

--- src/rpi_ai/functions.py
+++ src/rpi_ai/functions.py
@@ -1,8 +1,5 @@
 from rpi_ai.function_calling.devices.vybra_spire import VybraSpire
 from rpi_ai.function_calling.system_info import SystemInfo
-from rpi_ai.models.logger import Logger
-
-logger = Logger(__name__)
 
 FUNCTIONS = SystemInfo().functions
 FUNCTIONS.extend(VybraSpire().functions)
