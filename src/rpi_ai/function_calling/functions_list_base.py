c8581fd2814bf724719d3124250c518b1f118a0c
Javid Ahmed <javidahmed@icloud.com>
2025-07-05 01:43:23 +0100

    Refactor FunctionsListBase to implement iterable protocol and update FUNCTIONS to unpack SystemInfo directly

--- src/rpi_ai/function_calling/functions_list_base.py
+++ src/rpi_ai/function_calling/functions_list_base.py
@@ -1,11 +1,15 @@
 from abc import ABC, abstractmethod
-from collections.abc import Callable
+from collections.abc import Callable, Iterator
 
 
 class FunctionsListBase(ABC):
     def __init__(self) -> None:
         self.functions: list[Callable] = []
         self.setup_functions()
+
+    def __iter__(self) -> Iterator[Callable]:
+        """Make the class iterable so list() returns the functions attribute."""
+        return iter(self.functions)
 
     @abstractmethod
     def setup_functions(self) -> None:
--- src/rpi_ai/functions.py
+++ src/rpi_ai/functions.py
@@ -3,5 +3,5 @@ from rpi_ai.function_calling.system_info import SystemInfo
 system_info = SystemInfo()
 
 FUNCTIONS = [
-    *system_info.functions,
+    *system_info,
 ]
