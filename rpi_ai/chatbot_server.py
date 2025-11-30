c8c0ca923f49dbf3d81a540bad5fd0c150a747bc
Javid Ahmed <javidahmed@icloud.com>
2025-11-30 19:24:18 +0000

    Add authenticated routes for retrieving and updating chatbot configuration

--- rpi_ai/chatbot_server.py
+++ rpi_ai/chatbot_server.py
@@ -4,12 +4,18 @@ import logging
 import os
 from pathlib import Path
 
+from fastapi import Request
 from python_template_server.constants import CONFIG_DIR, CONFIG_FILE_NAME
+from python_template_server.models import ResponseCode
 from python_template_server.template_server import TemplateServer
 
 from rpi_ai.chatbot import Chatbot
 from rpi_ai.functions import FUNCTIONS
-from rpi_ai.models import ChatbotServerConfig
+from rpi_ai.models import (
+    ChatbotConfig,
+    ChatbotServerConfig,
+    GetConfigResponse,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -47,6 +53,8 @@ class ChatbotServer(TemplateServer):
     def setup_routes(self) -> None:
         """Set up API routes."""
         super().setup_routes()
+        self.add_authenticated_route("/config", self.get_config, GetConfigResponse, methods=["GET"])
+        self.add_authenticated_route("/config", self.post_config, None, methods=["POST"])
 
     @property
     def config_dir(self) -> Path:
@@ -61,6 +69,25 @@ class ChatbotServer(TemplateServer):
         logger.info("Config directory: %s", config_dir)
         return config_dir
 
+    async def get_config(self, request: Request) -> GetConfigResponse:
+        """Get current chatbot configuration."""
+        logger.info("Retrieving chatbot configuration...")
+        return GetConfigResponse(
+            code=ResponseCode.OK,
+            message="Successfully retrieved chatbot configuration.",
+            timestamp=GetConfigResponse.current_timestamp(),
+            config=self.chatbot.get_config(),
+        )
+
+    async def post_config(self, request: Request) -> None:
+        """Update chatbot configuration."""
+        logger.info("Updating chatbot configuration...")
+        self.config.chatbot_config = ChatbotConfig.model_validate(await request.json())
+        self.chatbot.update_config(self.config.chatbot_config)
+        logger.info("Saving updated configuration to file: %s", self.config_filepath)
+        self.config.save_to_file(self.config_filepath)
+        logger.info("Restarting chatbot with updated configuration...")
+        self.chatbot.start_chat()
 
 def run() -> None:
     """Serve the FastAPI application using uvicorn.
